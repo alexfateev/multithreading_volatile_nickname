65b73facc413d9cfb75164342cd8487c424cf0a3
alexfateev <49758927+alexfateev@users.noreply.github.com>
2023-10-10 19:48:30 +0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,42 @@ public class Main {
     static AtomicInteger length4 = new AtomicInteger(0);
     static AtomicInteger length5 = new AtomicInteger(0);
 
+    static boolean isPalindrom(String word) {
+        if (word.length() % 2 != 0) return false;
+        int i1 = 0;
+        int i2 = word.length() - 1;
+        while (i2 > i1) {
+            if (word.charAt(i1) != word.charAt(i2)) {
+                return false;
+            }
+            i1++;
+            i2--;
+        }
+        return true;
+    }
+
+    static boolean isSimilar(String word) {
+        char[] arr = word.toCharArray();
+        for (char c : arr) {
+            if (c != word.charAt(0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isAscending(String word) {
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)
+                    || word.charAt(i) - word.charAt(i - 1) == 1) {
+
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -25,22 +61,30 @@ public class Main {
 
         Thread thread1 = new Thread(() -> {
             for (int i = 0; i < texts.length; i++) {
-                if (texts[i].equals("aaa")) {
-                    length3.addAndGet(1);
+                if (texts[i].length() == 3) {
+                    if (isAscending(texts[i]) || isPalindrom(texts[i]) || isSimilar(texts[i])) {
+                        length3.addAndGet(1);
+                    }
                 }
             }
         });
+
         Thread thread2 = new Thread(() -> {
             for (int i = 0; i < texts.length; i++) {
-                if (texts[i].equals("abba")) {
-                    length4.addAndGet(1);
+                if (texts[i].length() == 4) {
+                    if (isAscending(texts[i]) || isPalindrom(texts[i]) || isSimilar(texts[i])) {
+                        length4.addAndGet(1);
+                    }
                 }
             }
         });
+
         Thread thread3 = new Thread(() -> {
             for (int i = 0; i < texts.length; i++) {
-                if (texts[i].equals("aaccc")) {
-                    length5.addAndGet(1);
+                if (texts[i].length() == 5) {
+                    if (isAscending(texts[i]) || isPalindrom(texts[i]) || isSimilar(texts[i])) {
+                        length5.addAndGet(1);
+                    }
                 }
             }
         });
